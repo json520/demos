fd607ca2672770adc8bfabdd3ef5bee8e11b51f0
方丈 <json4399@163.com>
2018-10-03 17:11:38 +0800

    添加checkSogou文件夹
    单独用于审查后台链接

---
+++ checkSogou/assets/linkData/sogou.js
@@ -0,0 +1,48 @@
+const sogouMediaName = '厦门钱牛网络借贷信息中介服务有限公司'
+let sogou = {
+    'message': 'xiamenqianniu00@qq.com-09',
+    'note': '搜狗SEM账户',
+    'code': '0',
+    'lists': [
+        {
+            'id': 0,
+            'title': '推广链接',
+            'src': 'http://xndk.jiyongqianbao.com/h5/activities_2017/20180910_xygj.html?utm_channel=xygj_sgssdk13',
+            'sogouAccount': 'xiamenqianniu00@qq.com',
+            'sogouPassword': '',
+            'customerName': '信用管家',
+            'mediaName': sogouMediaName,
+            '360Account': '',
+            'smAccount': '',
+            'ucAccount': '',
+            'baiduAccount': '',
+            'baiduFlowAccount': '',
+            'createDate': '20180901'
+        },
+        {
+            'title': '二级域名',
+            'src': 'http://xndk.jiyongqianbao.com',
+            'sogouAccount': 'xiamenqianniu00@qq.com',
+        },
+        {
+            'id': 1,
+            'title': '推广链接',
+            'src': 'http://xnsgdk1.shandianjieqian.cn/h5/activities_2018/20180612_xygj.html?launch_channel=xygj_zr_bdxxl&utm_channel=sgssdk30',
+            'sogouAccount': 'xiamenqianniu01@qq.com',
+            'sogouPassword': '',
+            'customerName': '信用管家',
+            'mediaName': sogouMediaName,
+            '360Account': '',
+            'smAccount': '',
+            'ucAccount': '',
+            'baiduAccount': '',
+            'baiduFlowAccount': '',
+            'createDate': '20180901'
+        },
+        {
+            'title': '二级域名',
+            'src': 'http://xnsgdk1.shandianjieqian.cn',
+            'sogouAccount': 'xiamenqianniu00@qq.com',
+        },
+    ]
+}
